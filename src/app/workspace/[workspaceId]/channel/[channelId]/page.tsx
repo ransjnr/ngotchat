19e90db5483a16a20c6bc5ff6898d2c246678aef
Ransford Oppong <roppong32@st.knust.edu.gh>
2024-09-16 13:53:35 -0700

    push

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -4,12 +4,17 @@ import { UseGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 import { Header } from "./header";
+import { ChatInput } from "./chat-input";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
+  const { results } = useGetMessages({ channelId });
   const { data: channel, isLoading: channelLoading } = UseGetChannel({
     id: channelId,
   });
+
+  console.log({ results });
 
   if (channelLoading) {
     return (
@@ -30,6 +35,8 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
+      <div className="flex-1">{JSON.stringify(results)}</div>
+      <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   );
 };
